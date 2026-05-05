91573ddd17abe12e1f43ef45fe2e10e7912e5e6e
pmonington <pmonington@trihydro.com>
2026-05-05 14:02:03 -0600

    refactor: use TLS strategy for connection manager instead of deprecated setSSLContext

--- jpo-security-svcs/src/main/java/us/dot/its/jpo/sec/helpers/HttpClientFactory.java
+++ jpo-security-svcs/src/main/java/us/dot/its/jpo/sec/helpers/HttpClientFactory.java
@@ -5,6 +5,10 @@ import javax.net.ssl.SSLContext;
 import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.DefaultClientTlsStrategy;
+import org.apache.hc.client5.http.ssl.TlsSocketStrategy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,7 +19,12 @@ public class HttpClientFactory {
             if (sslContext == null) {
                 return null;
             }
-            httpClientBuilder.setSSLContext(sslContext);
-            return httpClientBuilder.build();
+            TlsSocketStrategy tlsStrategy = new DefaultClientTlsStrategy(sslContext);
+
+            HttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
+                    .setTlsSocketStrategy(tlsStrategy)
+                    .build();
+
+            return httpClientBuilder.setConnectionManager(connectionManager).build();
         }
 }
